8293f94c2c6ca55887905134548f9ac3664d1638
Komal Sainath <bloomkoms@gmail.com>
2019-01-27 19:43:00 +0530

    upload doc

---
+++ src/BasisClass.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author komal
+ */
+public class BasisClass {
+public static int manu_id,itemcount,S_no,customerid,customer_purchases,med_code;
+public static  StringBuffer id=new StringBuffer();
+public static  StringBuffer pwd=new StringBuffer();
+
+}
